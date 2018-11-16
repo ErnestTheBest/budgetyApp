6db0c87f03167f136a2185302b1ca020ce897062
Ernests Pakers <ernests_pakers@rcomext.com>
2018-11-16 14:23:39 +0200

    Reading iput from fields. Storing it...

--- app.js
+++ app.js
@@ -5,8 +5,55 @@ let DOM = {
     inputDescription: '.add__description',
     inputValue: '.add__value'
 };
+
 let budgetController = (function () {
 
+    // Expense item constructor
+    let Expense = function (id, description, value) {
+        this.id = id;
+        this.descriptipon = description;
+        this.value = value;
+    };
+
+    // Income item constructor
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.descriptipon = description;
+        this.value = value;
+    };
+
+    // "DB" model to store all data
+    let data = {
+        allItems: {
+            expense: [],
+            income: []
+        },
+        totals: {
+            expense: 0,
+            income: 0
+        }
+    };
+
+    // Give global access to budget controller
+    return {
+        addItem: function (type, desc, val) {
+            let newItem, ID;
+            data.allItems[type].length > 0 ?
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1 :
+                ID = 0;
+
+            if (type === 'expense') {
+                newItem = new Expense(ID, desc, val);
+                ID++;
+            } else if (type === 'income') {
+                newItem = new Income(ID, desc, val);
+                ID++;
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        }
+    }
 })();
 
 let UIController = (function () {
@@ -23,6 +70,7 @@ let UIController = (function () {
 
 let controller = (function (budgetCtrl, UICtrl) {
 
+    // Setup event listeners for elements
     let setupEventListeners = function () {
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
@@ -34,12 +82,14 @@ let controller = (function (budgetCtrl, UICtrl) {
     };
 
     let ctrlAddItem = function () {
+        let input, newItem;
 
         // 1. Get input
-        let input = UICtrl.getInput();
+        input = UICtrl.getInput();
         console.log(input);
 
         // 2. Add item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add item to UI
 
